b798b97eaed9ba8b8399e6a23436a5daa21c1ab9
Trevor Delker <tddelker@gmail.com>
2019-08-08 12:58:03 -0600

    a felony

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
+let mysql = require('mysql');
+let inquirer = require('inquirer');
 let Table = require('cli-table2');
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
   host:"localhost",
   port: 3306,
   user:'root',
@@ -15,20 +15,20 @@ connection.connect(function(err){
 	console.log("connected as id" + connection.threadId);
 });
 
-var displayProducts = function(){
-	var query = "Select * FROM products";
+let displayProducts = function(){
+	let query = "Select * FROM products";
 	connection.query(query, function(err, res){
-		if(err) throw err;
-		var displayTable = new Table ({
-			head: ["Item ID", "Product Name", "Category", "Price", "Quantity"],
-			colWidths: [10,25,25,10,14]
+	if(err) throw err;
+	var tblDisply = new Table ({
+	head: ["Item ID", "Product Name", "Category", "Price", "Quantity"],
+	colWidths: [10,25,25,10,14]
 		});
 		for(var i = 0; i < res.length; i++){
-			displayTable.push(
+			tblDisply.push(
 				[res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
 				);
 		}
-		console.log(displayTable.toString());
+		console.log(tblDisply.toString());
 		purchasePrompt();
 	});
 }
@@ -49,8 +49,8 @@ function purchasePrompt(){
 	},
 
  ]).then(function(answers){
- 	var neededQuantity = answers.Quantity;
- 	var requestID = answers.ID;
+ 	let neededQuantity = answers.Quantity;
+ 	let requestID = answers.ID;
  	purchaseOrder(requestID, neededQuantity);
  });
 };
@@ -59,7 +59,7 @@ function purchaseOrder(ID, amtNeeded){
 	connection.query('Select * FROM products WHERE ?',{item_id: ID}, function(err,res){
 		if(err){console.log(err)};
 		if(amtNeeded <= res[0].stock_quantity){
-			var productCost = res[0].price * amtNeeded;
+			let productCost = res[0].price * amtNeeded;
 			console.log("Order in stock.");
 			console.log("Your total cost for " + amtNeeded + " " +res[0].product_name + " is " + productCost + " Please come again!");
 			let updatedQuantity = res[0].stock_quantity - amtNeeded
